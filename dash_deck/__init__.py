720fb7cbda8cc9fd25630df77f6251ded907647d
Natalie Fernandes Spanghero <sg_nata@msn.com>
2025-04-07 08:53:24 -0300

    Minhas alterações locais

--- dash_deck/__init__.py
+++ dash_deck/__init__.py
@@ -1,6 +1,20 @@
 from ._imports_ import *  # importa os componentes React
 from ._metadata import PACKAGE_NAME, __version__
 
-__all__ = [
-    "DeckGL"
+__all__ = ["DeckGL"]
+
+_js_dist = [
+    {
+        "relative_package_path": "dash_deck.min.js",
+        "namespace": PACKAGE_NAME
+    },
+    {
+        "relative_package_path": "dash_deck.min.js.map",
+        "namespace": PACKAGE_NAME,
+        "dynamic": True
+    }
 ]
+
+for component in __all__:
+    setattr(locals()[component], "_js_dist", _js_dist)
+
